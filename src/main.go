58115928b3d738700d29091a13f0f69efadda6d4
Victor Ivan <viictor.ivan@gmail.com>
2023-10-02 20:35:45 -0300

    chore: removing sheeets

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/joho/godotenv"
 	"kddw.article.service/src/controllers"
 	"kddw.article.service/src/db"
-	"kddw.article.service/src/domain/entities"
 	"kddw.article.service/src/domain/services"
 	"kddw.article.service/src/repositories"
 	"kddw.article.service/src/server"
@@ -25,12 +24,6 @@ func main() {
 	db.Migrate(database.Db)
 
 	repos := repositories.NewRepositories(database)
-
-	repos.ArticleRepo.CreateArticle(&entities.Article{
-		Title:    "Article 1",
-		Content:  "Content 1",
-		AuthorId: 123,
-	})
 
 	s := services.NewServices(repos)
 	c := controllers.NewControllers(s)
